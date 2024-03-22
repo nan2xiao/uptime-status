1a82a9c810d17da0cde80c348f2a07139d1cbb84
aran0519 <aran0519@126.com>
2024-03-22 13:01:28 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,30 @@
+window.Config = {
+  // 显示标题
+  SiteName: 'Uptime Status 统计',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm796605247-38b33d2a085432202f956e6c',
+    'm796605299-6c5fec11c89937039348086d',
+    'm796605301-851253a46f0b387f40df9260',
+    'm796606086-3a8126746e0039fb403f7eb0',
+    'm796608610-be672d7d88a594d6c3f85b21',
+    'm796613448-6613beab03d67eac7383d5d6',
+    'm796605332-9b26612bd660d759cedc1c66',
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+//    {
+//      text: 'Homepage',
+//      url: 'https://status.org.cn/'
+//    },
+  ],
+};
