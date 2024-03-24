091620d988e1ed5cdff4fc9bc5248de22b436115
aran0519 <aran0519@126.com>
2024-03-25 00:13:29 +0800

    123

--- config.js
+++ config.js
@@ -5,13 +5,14 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm796605247-38b33d2a085432202f956e6c',
-    'm796605299-6c5fec11c89937039348086d',
-    'm796605301-851253a46f0b387f40df9260',
-    'm796606086-3a8126746e0039fb403f7eb0',
-    'm796608610-187cb937409fc821f4f542de',
-    'm796613448-6613beab03d67eac7383d5d6',
-    'm796605332-9b26612bd660d759cedc1c66',
+    'm796605247-38b33d2a085432202f956e6c', // 墨离的主页
+    'm796605299-6c5fec11c89937039348086d', // Upload 图床    
+    'm796605301-851253a46f0b387f40df9260', // Short 短链
+    'm796627788-675e8cb3713d4b8371824a96', // Bing Wallpaper 壁纸接口
+    'm796606086-3a8126746e0039fb403f7eb0', // Uptime Status 统计
+    'm796608610-187cb937409fc821f4f542de', // 外部 Telegraph 服务
+    'm796613448-6613beab03d67eac7383d5d6', // 第三方 UptimeRobot 接口服务
+    'm796605332-9b26612bd660d759cedc1c66', // Example 测试
   ],
 
   // 日志天数
