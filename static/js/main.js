76b7404423298394e6389c8a8401a88e977b928b
nan2xiao <3134955652@qq.com>
2024-04-11 23:55:33 +0800

    123

--- static/js/main.js
+++ static/js/main.js
@@ -10340,7 +10340,8 @@ n.n = function(e) {
 			xn = "2.0.0";
 		var _n = function() {
 			var t = (0, e.useMemo)((function() {
-				var e = window.Config.ApiKeys;
+//				var e = window.Config.ApiKeys;
+				var e = env.API_KEY;
 				return Array.isArray(e) ? e : "string" === typeof e ? [e] : []
 			}), []);
 			return (0, i.jsxs)(i.Fragment, {
