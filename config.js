5a61fae1e0e76ef61112ed25c137936491031556
nan2xiao <3134955652@qq.com>
2024-04-12 00:07:09 +0800

    123

--- config.js
+++ config.js
@@ -4,19 +4,7 @@ window.Config = {
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
-  ApiKeys: [
-    'm796605247-38b33d2a085432202f956e6c', // 墨离的主页
-    'm796629484-ac42a469caa3372818994912', // 墨离的博客
-    'm796605299-6c5fec11c89937039348086d', // Upload 图床    
-    'm796605301-851253a46f0b387f40df9260', // Short 短链
-    'm796670171-0a7567e041c57d2b23e68257', // Short 短链 - 短域
-    'm796670135-b7b7832993c55b5a0a4e323a', // Short 短链 - 直链域
-    'm796627788-675e8cb3713d4b8371824a96', // Bing Wallpaper 壁纸接口
-    'm796606086-3a8126746e0039fb403f7eb0', // Uptime Status 统计
-    'm796608610-187cb937409fc821f4f542de', // 外部 Telegraph 服务
-    'm796613448-6613beab03d67eac7383d5d6', // 第三方 UptimeRobot 接口服务
-    'm796605332-9b26612bd660d759cedc1c66', // Example 测试
-  ],
+  ApiKeys: [],
 
   // 日志天数
   CountDays: 28,
@@ -32,3 +20,13 @@ window.Config = {
 //    },
   ],
 };
+
+// 从环境变量中读取 ApiKeys
+const apiKeysString = env.API_KEYS;
+if (apiKeysString) {
+  // 使用逗号分割字符串，并去除空格
+  const apiKeysArray = apiKeysString.split(',').map(apiKey => apiKey.trim());
+
+  // 将拆分后的 ApiKeys 填充到 Config 中
+  window.Config.ApiKeys = apiKeysArray;
+}
