99f0fb37fa3a23d0dd68d306b840659a423228b1
暔晓 <159799757+nan2xiao@users.noreply.github.com>
2024-05-30 18:51:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,17 +5,9 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm796605247-38b33d2a085432202f956e6c', // 墨离的主页
-    'm796629484-ac42a469caa3372818994912', // 墨离的博客
-    'm796605299-6c5fec11c89937039348086d', // Upload 图床    
-    'm796605301-851253a46f0b387f40df9260', // c1nTop 短链
-    'm796670171-0a7567e041c57d2b23e68257', // c1nTop 短链 - 短域
-    'm796670135-b7b7832993c55b5a0a4e323a', // c1nTop 短链 - 直链域
-    'm796627788-675e8cb3713d4b8371824a96', // Bing Wallpaper 壁纸接口
-    'm796606086-3a8126746e0039fb403f7eb0', // Uptime Status 统计
-    'm796608610-187cb937409fc821f4f542de', // 外部 Telegraph 服务
-    'm796613448-6613beab03d67eac7383d5d6', // 第三方 UptimeRobot 接口服务
-    'm796605332-9b26612bd660d759cedc1c66', // Example 测试
+    'm796605247-38b33d2a085432202f956e6c',
+    'm796629484-ac42a469caa3372818994912',
+    'm796605299-6c5fec11c89937039348086d'
   ],
 
   // 日志天数
@@ -30,5 +22,5 @@ window.Config = {
 //      text: '',
 //      url: ''
 //    },
-  ],
+  ]
 };
